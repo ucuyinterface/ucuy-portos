04d22ce13cd067a45e972e0653eca3ff936c7885
ucuyinterface <acopyofaan@gmail.com>
2024-12-15 22:07:23 +0700

    setup:initial-setup

---
+++ layout.tsx
@@ -0,0 +1,24 @@
+import { ThemeProvider } from "./providers"
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <head />
+      <body>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  )
+}
+
